2effc89ca5278d26926f3dc97d9c9c1835d04ebc
WolfgangSiegert <WoSiegert@gmx.de>
2020-05-08 16:14:21 +0200

    Add a POST on submit event

--- src/views/CreateEvent.vue
+++ src/views/CreateEvent.vue
@@ -14,6 +14,7 @@
 </template>
 
 <script>
+import myAxios from "axios";
 export default {
   data() {
     return {
@@ -25,7 +26,15 @@ export default {
   methods: {
     submit() {
       console.log(this.eventName);
-      alert(this.eventName.title);
+      //alert(this.eventName.title);
+      myAxios
+        .post("http://localhost:3000/events", this.eventName)
+        .then(res => {
+          console.log("Created new Event", res.data);
+        })
+        .catch(error => {
+          console.error("The server call didnt work: ", error);
+        });
     }
   }
 };
