3c8e8fef5fcb5ac0a7ecdee7c5fb76c3ad117f2b
WolfgangSiegert <WoSiegert@gmx.de>
2020-05-12 15:50:20 +0200

    Add getEvents() and getEvent(id) to event-service

--- src/services/event-service.js
+++ src/services/event-service.js
@@ -3,3 +3,9 @@ import axios from "axios";
 export function createEvent(event) {
   return axios.post("http://localhost:3000/events", event);
 }
+export function getEvents() {
+  return axios.get("http://localhost:3000/events");
+}
+export function getEvent(id) {
+  return axios.get(`http://localhost:3000/events/${id}`);
+}
